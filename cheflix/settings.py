6d6fa3b9d9df2503b78c5d880c101825fb7b5f20
Yakkim <joaquincalvo293@gmail.com>
2026-06-25 15:55:34 -0300

    cambios seguridad

--- cheflix/settings.py
+++ cheflix/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from dotenv import load_dotenv
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uv_bowxz8%39d&^z3%e8puizu(9z9g%909+e@ah=v0fhjk$@5%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,11 +123,12 @@ LOGIN_REDIRECT_URL = '/'
 
 #ApiKey OMDb
 OMDB_API_KEY = 'fac330c4'
+load_dotenv()
 
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = 'joaquincalvo111@gmail.com'      # tu Gmail
-EMAIL_HOST_PASSWORD = 'qrillbsztddctpnx'     # la clave de 16 caracteres
-DEFAULT_FROM_EMAIL  = 'joaquincalvo111@gmail.com'
+EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL  = os.getenv('EMAIL_HOST_USER')
